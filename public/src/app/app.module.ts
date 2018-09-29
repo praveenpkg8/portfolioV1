759516a398a31ddf4d4e41398abca9e6172d2266
Praveen Kumar <praveenpkg8@gmail.com>
2018-09-30 03:32:17 +0530

    Initial commit

---
+++ public/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router'
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+    ProjectComponent,
+    ContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent},
+      { path: 'home', component: HomeComponent},
+      { path: 'about', component: AboutComponent},
+      { path: 'project', component: ProjectComponent},
+      { path: 'contact', component: ContactComponent},
+
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
